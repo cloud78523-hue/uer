692b480ae7a7d81870663ee31ab93db7df0ab2b8
uer <cloud78523@mail.com>
2025-10-19 13:04:21 +0800

    [App] 新增註解

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -18,10 +18,15 @@ public class Ball : MonoBehaviour
     // 每幀執行一次
     void Update()
     {
-        // Debug.Log("物件啟用時間"+Time.time);
+        // Debug.Log("物件啟用時間" + Time.time);
 
+        // 移動物件Z軸0.01單位
+        // transform.position = transform.position + new Vector3(0, 0, 0.01f);
+        // transform.position += new Vector3(0, 0, 0.01f);
         // transform.Translate(0, 0, 0.01f);
 
+
+        // 取得垂直和水平方向的輸入並移動
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
         transform.Translate(h * Time.deltaTime, 0, v * Time.deltaTime);
