480760a7d05cbe0894bec1ac333df6d9edb38c0b
uer <cloud78523@mail.com>
2025-10-19 12:54:25 +0800

    [Add]新增專案、小球移動

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+     void Awake()
+    {
+
+       // Debug.Log("遊戲初始化");
+    }
+    // 物件啟用時執行一次
+    void Start()
+    {
+        Debug.Log("物件啟用");
+    }
+
+    // 每幀執行一次
+    void Update()
+    {
+        // Debug.Log("物件啟用時間"+Time.time);
+
+        // transform.Translate(0, 0, 0.01f);
+
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        transform.Translate(h * Time.deltaTime, 0, v * Time.deltaTime);
+    }
+}
