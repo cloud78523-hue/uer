de96182624fc31437966c8d1e278c98ab6e6c239
cloud78523 <cloud78523@gmail.com>
2025-10-25 14:04:44 +0800

    小球跳躍 金幣碰撞 障礙物移動

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-     void Awake()
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 380f;
+    void Awake()
     {
-
+         
        // Debug.Log("遊戲初始化");
     }
     // 物件啟用時執行一次
@@ -29,6 +31,12 @@ public class Ball : MonoBehaviour
         // 取得垂直和水平方向的輸入並移動
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
-        transform.Translate(h * Time.deltaTime, 0, v * Time.deltaTime);
+        transform.Translate(h * Time.deltaTime * speed, 0, v * Time.deltaTime * speed);
+
+        // 跳躍
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(0, jumpForce, 0);
+        }
     }
 }
---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] private Transform target; // 要追蹤的物件
+
+    private Vector3 distance;                  // 相機與目標的距離
+
+    void Start()
+    {
+        distance = target.position - transform.position; // 計算初始距離
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform .position = target.position - distance; 
+    }
+}
